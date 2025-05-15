2a9313a7e832b83ae25ff11daa35cdace97e9b6a
Peanut-leaf <maalogal@stu.otc.school.nz>
2025-05-16 10:56:09 +1200

    finished first round of dictionaries

--- takeitaway.py
+++ takeitaway.py
@@ -1,13 +1,13 @@
+""" Hello, this is my takeaway program for a fast food servoce called 'take it away' """
 totalcost = 0
 finalorder = {}
 chickenbuckets = {'20pc': 49.99, '12pc': 29.99, '10pc': 24.99, '6pc': 14.99 }
-burgers = {}
-sides = {}
-drinks = {}
+burgers = {'tower': 14.99,'zinger': 12.99, 'mcspicy': 11.99, 'mcchicken': 8.99, '': 4.99 }
+sides = {'chips': 4.99, 'potato and gravy': 4.99, 'coleslaw': 3.99, 'nuggets': 6.99 }
+drinks = {'coke': 2.50, 'sprite': 2.50, 'fanta': 2.50, 'lmp': 2.50}
+sauses = {'bbq': 0.99, 'tomato': 0.99, 'garlic aoli': 0.99}
 
-
-
-
+alergymes = "This item contains ingredients that could cause alergens"
 
 
 
