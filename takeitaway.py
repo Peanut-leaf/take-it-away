a4cd24882c549bcc2cdd75d8ccc3f431bf36fdf4
Peanut-leaf <maalogal@stu.otc.school.nz>
2025-08-07 13:28:37 +1200

    finished up small details and refined the code.

--- takeitaway.py
+++ takeitaway.py
@@ -1,15 +1,20 @@
 """Hello, this is my takeaway program for a fast food service called take it away """
+
+# Importing nescessary libraries
 from tkinter import*
 from tkinter import ttk
 from tkinter import font
 from PIL import Image, ImageTk 
-import os
 import random
 from datetime import date
 from datetime import datetime
 
-
+# -------------------- Global Variables --------------------
 Foodprices = {"Chicken Bucket": 30, "Chicken Pieces": 5, "Chicken Burger": 10, "Crispy Chips": 3, "Saucy Sauce": 1, "Fizzy Drink": 2}
+new_reciept = ""
+
+def disable():
+    pass
 
 #Creates and places window on screen
 login_window = Tk()
@@ -18,24 +23,26 @@ login_window.geometry("340x340")
 login_window.title("Take it away - Login Page")
 login_window.iconphoto(True, icon)  # Set the window icon
 
-
-login_window.config(background= "#e4042c") 
+#Add background colour, make the window not resizable, bind basic key commands, and disable close button for user.
+login_window.config(background= "#355aa9") 
 login_window.resizable(False, False)
 login_window.bind("<Return>", lambda event: login()) 
-login_window.bind("<Escape>", lambda event: login_window.destroy())
+login_window.bind("<End>", lambda event: login_window.destroy())
 login_window.bind("<FocusIn>", lambda event: login_instruction_label.config(text="Please enter your email and password"))
+login_window.protocol("WM_DELETE_WINDOW", disable)
 
 
 def login():
     email = email_entry.get()
     password = password_entry.get()
     name = name_entry.get()
-    if "@" not in email or "." not in email:  
+    if email and password == "":
+        login_instruction_label.config(text="Please enter valid email and password to continue")
+    elif "@" not in email or "." not in email:  
         login_instruction_label.config(text="Please enter a correct email address.")
     elif len(password) < 6:  
         login_instruction_label.config(text="Password be at least 6 letters long.")
-    elif email and password == "":
-        login_instruction_label.config(text="Please enter valid email and password to continue")
+
     else:
         login_instruction_label.config(text=f"Welcome {name}! You are now logged in.")
         
@@ -44,12 +51,12 @@ def login():
 
 
 # -------------------- Creating the login frame --------------------
-login_frame = Frame(background = "#e4042c")  
+login_frame = Frame(background = "#355aa9")  
 
 # -------------------- Labels for the login page --------------------
 welcome_label = Label(login_frame, 
     text="Welcome to Take it away!", 
-    background= "#e4042c", 
+    background= "#355aa9", 
     foreground= "black", 
     font=("Verdana", 9, "bold"),
     relief=GROOVE,
@@ -57,25 +64,25 @@ welcome_label = Label(login_frame,
  
 email_label = Label(login_frame, 
     text = "Email:", 
-    background="#e4042c", 
+    background="#355aa9", 
     foreground= "black", 
     font=("Verdana", 12))
 
 password_label = Label(login_frame, 
     text = "Password:", 
-    background= "#e4042c", 
+    background= "#355aa9", 
     foreground= "black", 
     font=("Verdana", 12))
 
 name_label = Label(login_frame,  
     text = "Name:", 
-    background= "#e4042c", 
+    background= "#355aa9", 
     foreground= "black", 
     font=("Verdana", 12))
     
 login_instruction_label = Label(login_frame, 
     text="Please enter your email and password to continue.", 
-    background= "#ff0000", 
+    background= "#355aa9", 
     foreground= "black", 
     font=("Verdana", 10, "bold"), 
     relief=GROOVE,
@@ -90,7 +97,7 @@ name_entry = ttk.Entry(login_frame, font=("Verdana", 12))
 
 login_button = Button(login_frame, 
     text="Login", 
-    background= "#e4042c", 
+    background= "#355aa9", 
     foreground= "black", 
     font=("verdana", 12, "bold"), 
     command=login)
@@ -109,18 +116,22 @@ login_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
 
 login_window.mainloop()
 
+# ------------------------------ MENU WINDOW ------------------------------
+
+# After logging in, if statment checks if they are then crates a new window for the menu
 if login_window != None: 
     menu_window = Tk()  
     menu_window.geometry("940x546")  
     menu_window.title("Take it away - Menu")  
     menu_window.config(background="#355aa9")  
     menu_window.resizable(False, False)
-    menu_window.bind("<Escape>", lambda event: menu_window.destroy()) 
+    menu_window.bind("<End>", lambda event: menu_window.destroy()) 
+    menu_window.protocol("WM_DELETE_WINDOW", disable)
     icon = PhotoImage(file="iconlogo.png")
  
 # -------------------- Styling --------------------
-style = ttk.Style()
-style.theme_use("clam")  
+# Creating styles for the frames to make them more appealing and easier for the users to read
+style = ttk.Style() 
 style.configure("MainMenuFrame.TFrame", background="#1d284b")
 style.configure("MenuFrame.TFrame", background="#355aa9")
 style.configure("FoodDisplayFrame.TFrame", background="#111924")
@@ -133,9 +144,8 @@ style.configure('orderTotalLabel.TLabel', background = "#111924", font = ("Verda
 style.configure('orderTransaction.TLabel', background = "#93949a", font = ('Verdana', 12), foreground = "white", 
                 wraplength = 200, anchor = "nw", padding = (3, 3, 3, 3))
 
-
-
 # -------------------- Functions --------------------
+# Functions for displaying individual food items and updating the display label with the correct image and text
 def displaychickenbucket():
     ChickenbucketFrame.configure(relief = GROOVE, style = "SelectedFood.TFrame")
     
@@ -145,7 +155,6 @@ def displaychickenbucket():
     ChipsFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     SauceFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     DrinkFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = ChickenbucketImage, text = "Chicken Bucket", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
     
@@ -158,7 +167,6 @@ def displaychickenpiece():
     ChipsFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     SauceFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     DrinkFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = ChickenPieceImage, text = "Chicken Pieces", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
     
@@ -170,7 +178,6 @@ def displaychickenburger():
     ChipsFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     SauceFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     DrinkFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = BurgerImage, text = "Chicken Burger", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
     
@@ -182,7 +189,6 @@ def displayChips():
     BurgerFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     SauceFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     DrinkFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = ChipsImage, text = "Crispy Chips", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
     
@@ -194,7 +200,6 @@ def displaySauce():
     BurgerFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     ChipsFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     DrinkFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = SauceImage, text = "Saucy Sauce", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
     
@@ -206,11 +211,10 @@ def displayDrink():
     BurgerFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     ChipsFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
     SauceFrame.configure(relief = GROOVE, style = "FoodFrame.TFrame")
-
     displayLabel.configure(image = DrinkImage, text = "Fizzy Drink", style = "MenuLabel.TLabel",
                            compound = "bottom", foreground= "white", padding = (5, 5, 5, 5))
 
-
+#Add function that allows user 
 def add():
     current_order = orderTransaction.cget("text")
     selected_food = displayLabel.cget("text") + " - $" + str(Foodprices[displayLabel.cget("text")])
@@ -223,9 +227,11 @@ def add():
     # Update the total price
     order_total = orderTotalLabel.cget("text").replace("TOTAL : ", "")
     order_total = order_total.replace("$", "")
-    updated_total = int(order_total) + Foodprices[displayLabel.cget("text")]
+    order_total = int(order_total) if order_total.isdigit() else 0
+    updated_total = order_total + Foodprices[displayLabel.cget("text")]
     orderTotalLabel.configure(text="TOTAL : " + str(updated_total) + "$")
 
+# Remove function that allows user to remove unwanted food from their order where ever it is. eg first item or middle of the order
 def remove():
     current_order = orderTransaction.cget("text")
     displayed_food = displayLabel.cget("text")  
@@ -250,6 +256,7 @@ def remove():
     else:
         orderTransaction.configure(text="")
 
+#Generates a random order ID for the user to identify their order
 def generate_order_id():
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -275,7 +282,8 @@ def order():
     #Create a reciept file with user's total order price and date times
     with open(new_reciept, "w") as file:
         file.write("Take it away" + "\n")
-        file.write("------------------------------------" + "\n")
+        file.write(new_reciept + "\n")
+        file.write("----------------------------------------------------" + "\n")
         file.write(order_day.strftime("%x") + "\n")
         file.write(order_time.strftime("%H:%M:%p") + "\n\n")
         file.write("Items Ordered:"+ "\n")
@@ -285,51 +293,48 @@ def order():
         file.write(orderTotalLabel.cget("text") + "\n")
     
     #Reset the order display for the next customer. 
+    order_comfirmation = ("Your order reciept is"+ "\n" + new_reciept + "\n")
     orderTotalLabel.configure(text = "Total : $0")
     orderIDLabel.configure(text = "ORDER ID : " + generate_order_id())
-    orderTransaction.configure(text = "")
-
-
-
+    orderTransaction.configure(text = order_comfirmation)
+  
 # -------------------- Images --------------------
 
 #Logo to set the theme of the menu
 LogoImageObject = Image.open("Images/menu logo.png").resize((130, 130))
 LogoImage = ImageTk.PhotoImage(LogoImageObject)
 
-
 TopBannerImageObject = Image.open("Images/Top banner options/topimage2.jpg").resize((800, 130))
 TopBannerImage = ImageTk.PhotoImage(TopBannerImageObject)
 
 # Deafult image for when no food is selected
-PlaceholderimageObject = Image.open("Images/place holder v3.jpg").resize((350, 360))
+PlaceholderimageObject = Image.open("Images/place holder v3.jpg").resize((350, 368))
 PlaceholderImage = ImageTk.PhotoImage(PlaceholderimageObject)
-
 
 # -------------------- Food Images --------------------
 
-# Food images are smaller than the display image because they have an order button below
-ChickenbucketImageObject = Image.open("Images/food images/Chicken bucket.png").resize((350, 330))
+# Food images just smaller than display image beacause of the food name above the image
+ChickenbucketImageObject = Image.open("Images/food images/Chicken bucket.png").resize((350, 350))
 ChickenbucketImage = ImageTk.PhotoImage(ChickenbucketImageObject)
 
-
-ChickenPieceImageObject = Image.open("Images/food images/Chicken pieces.png").resize((350, 330))
+ChickenPieceImageObject = Image.open("Images/food images/Chicken pieces.png").resize((350, 350))
 ChickenPieceImage = ImageTk.PhotoImage(ChickenPieceImageObject)
 
-BurgerImageObject = Image.open("Images/food images/Chicken burger.png").resize((350, 330))
+BurgerImageObject = Image.open("Images/food images/Chicken burger.png").resize((350, 350))
 BurgerImage = ImageTk.PhotoImage(BurgerImageObject)
 
-ChipsImageObject = Image.open("Images/food images/chippies.png").resize((350, 330))
+ChipsImageObject = Image.open("Images/food images/chippies.png").resize((350, 350))
 ChipsImage = ImageTk.PhotoImage(ChipsImageObject)
 
-SauceImageObject = Image.open("Images/food images/Sause.png").resize((350, 330))
+SauceImageObject = Image.open("Images/food images/Sause.png").resize((350, 350))
 SauceImage = ImageTk.PhotoImage(SauceImageObject)
 
-DrinkImageObject = Image.open("Images/food images/El Drinco.png").resize((350, 330))
+DrinkImageObject = Image.open("Images/food images/El Drinco.png").resize((350, 350))
 DrinkImage = ImageTk.PhotoImage(DrinkImageObject)
 
-
 # -------------------- Frames --------------------
+
+# Each individual frame is created for all of the sections like order frame displaying total order and menu frame displaing foods
 mainFrame = ttk.Frame(menu_window, width= 800, height = 580, style="MainMenuFrame.TFrame")
 mainFrame.grid(row=0, column=0, sticky="NSEW")
 
@@ -345,9 +350,8 @@ displayFrame.grid(row=1, column=1, padx = 3, pady = 3, sticky="NSEW")
 OrderFrame = ttk.Frame(mainFrame, style="OrderFrame.TFrame")
 OrderFrame.grid(row=1, column=2, padx = 3, pady = 3, sticky="NSEW")
 
-
-
 # -------------------- Food Frames --------------------
+# Each food has its own frame to display different food item and prices, while also being able to be individually manipulated
 ChickenbucketFrame = ttk.Frame(menuFrame, style="FoodFrame.TFrame")
 ChickenbucketFrame.grid(row=1, column=0, sticky="NSEW")
 
@@ -392,11 +396,9 @@ orderTotalLabel.grid(row = 3, column = 0, sticky = "EW")
 orderButton = ttk.Button(OrderFrame, text = "ORDER", command= order)
 orderButton.grid(row = 4, column = 0, sticky = "EW")
 
-
 displayLabel = ttk.Label(displayFrame, image = PlaceholderImage, style="MenuLabel.TLabel")
 displayLabel.configure(background = "#111924")
 displayLabel.grid(row = 0, column = 0 , sticky = "NSEW", columnspan = 2)
-
 
 addOrderButton = ttk.Button(displayFrame, text = "ADD TO ORDER", command = add)
 addOrderButton.grid(row = 1, column = 0, padx = 2, sticky = "NSEW")
@@ -411,11 +413,9 @@ LogoLabel.grid(row = 0, column = 0, sticky = "W")
 RestaurantBannerLabel = Label(topBannerFrame, image = TopBannerImage, background = "#111924")
 RestaurantBannerLabel.grid(row = 0, column = 1, sticky = "NSEW")
 
-
 MainMenuLabel = ttk.Label(menuFrame, text = "MENU", style = "MenuLabel.TLabel")
 MainMenuLabel.grid(row = 0, column = 0, sticky = "WE")
 MainMenuLabel.configure(anchor = "center", font = ("Verdana", 14, "bold"))
-
 
 ChickenbucketLabel = ttk.Label(ChickenbucketFrame, text="Chicken Buckets     $30", style="MenuLabel.TLabel")
 ChickenbucketLabel.grid(row=0, column=0, padx=10, pady=10 ,sticky="w")
@@ -455,6 +455,7 @@ DrinkDisplayButton = ttk.Button(DrinkFrame, text ="Display", command= displayDri
 DrinkDisplayButton.grid(row = 0, column = 1, padx = 10)
 
 # -------------------- Configuring Grids --------------------
+# Configuring the grid weights to make frames expand and fill even amounts of avalible space
 mainFrame.columnconfigure(2, weight = 1)
 mainFrame.rowconfigure(1, weight = 1)
 menuFrame.columnconfigure(0, weight = 1)
